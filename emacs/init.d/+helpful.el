fe494af913f07a6a27f884b2d261be29a17a5aab
David Leal <dleal@mojotech.com>
2022-12-22 20:17:42 +0000

    [emacs] Add helpful

---
+++ emacs/init.d/+helpful.el
@@ -0,0 +1,29 @@
+;;; init --- Help configuration -*- lexical-binding: t; -*-
+;;;
+;;; Commentary:
+;;;
+;;; No comment.
+;;;
+;;; Code:
+
+(use-package helpful
+  :general
+  (:states '(normal visual motion)
+   :prefix "SPC"
+   "h"  '(nil :wk "help")
+   "h." '(helpful-at-point :wk "at point")
+   "hb" '(embark-bindings :wk "bindings")
+   "he" '(describe-face :wk "face")
+   "hf" '(helpful-callable :wk "command or function")
+   "hF" '(helpful-function :wk "function")
+   "hi" '(info :wk "info")
+   "hl" '(helpful-command :wk "command")
+   "hk" '(helpful-key :wk "key")
+   "hm" '(woman :wk "man page")
+   "ho" '(describe-mode :wk "mode")
+   "hv" '(helpful-variable :wk "variable"))
+  (:keymaps 'emabark-symbol-map
+   "h" 'helpful-at-point))
+
+(provide '+helpful)
+;;; +helpful.el ends here
