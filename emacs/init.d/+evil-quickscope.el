e215cd483e47b6b51d12f183d3e34e2728a1b63f
David Leal <dleal@mojotech.com>
2023-01-31 17:07:48 +0000

    [emacs/evil] Remove evil-quickscope

--- emacs/init.d/+evil-quickscope.el
+++
@@ -1,8 +0,0 @@
-;; -*- lexical-binding: t; -*-
-
-(use-package evil-quickscope
-  :custom
-  (evil-quickscope-word-separator " -_()\"\'")
-  :hook (prog-mode . turn-on-evil-quickscope-always-mode))
-
-(provide '+evil-quickscope)
