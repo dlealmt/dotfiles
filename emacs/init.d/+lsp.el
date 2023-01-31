4419899792c76b54f0e850c7c295a63396e800c2
David Leal <dleal@mojotech.com>
2023-01-31 16:12:24 +0000

    [emacs/lsp] Fix symbol passed to flycheck

--- emacs/init.d/+lsp.el
+++ emacs/init.d/+lsp.el
@@ -3,7 +3,7 @@
 (use-package lsp-mode
   :custom
   (lsp-diagnostic-clean-after-change t)
-  (lsp-diagnostics-flycheck-default-level 'ẃarning)
+  (lsp-diagnostics-flycheck-default-level 'warning)
   (lsp-headerline-breadcrumb-enable nil)
   (lsp-solargraph-use-bundler t)
   (lsp-keymap-prefix "s-@"))
